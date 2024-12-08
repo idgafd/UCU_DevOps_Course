dedac0dfbe6b11a4ee6abc45b5599871faa21fb2
Anastasiya Mazur <98967429+idgafd@users.noreply.github.com>
2024-12-08 06:17:12 +0200

    Update test_hello.py

--- github-actions-samples/app/test_hello.py
+++ github-actions-samples/app/test_hello.py
@@ -1,8 +1,33 @@
-from hello import app
+import pytest
+from hello import app, get_message
+from unittest.mock import patch
 
 
-def test_hello():
-    response = app.test_client().get('/')
+def test_get_message_anastasiia():
+    with patch("hello.configparser.RawConfigParser.getboolean", return_value=True):
+        assert get_message() == "Hello, Anastasiia!"
 
-    assert response.status_code == 200
-    # more and more tests to be here
+
+def test_get_message_world():
+    with patch("hello.configparser.RawConfigParser.getboolean", return_value=False):
+        assert get_message() == "Hello, World!"
+
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+
+def test_hello_endpoint_anastasiia(client):
+    with patch("hello.configparser.RawConfigParser.getboolean", return_value=True):
+        response = client.get("/")
+        assert response.status_code == 200
+        assert response.data.decode() == "Hello, Anastasiia!"
+
+
+def test_hello_endpoint_world(client):
+    with patch("hello.configparser.RawConfigParser.getboolean", return_value=False):
+        response = client.get("/")
+        assert response.status_code == 200
+        assert response.data.decode() == "Hello, World!"
